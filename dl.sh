0a2d920cf52737f07b31ba890f20183a0747cf98
Ayoub Hebbaj <30575106+M3nti0nL3Ss@users.noreply.github.com>
2020-01-23 20:01:55 +0100

    main script

---
+++ dl.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+clear
+echo "-------By-M3nti0nL3Ss-------"
+echo "Enter youtube urls: "
+x="N"
+touch dl
+while [[ $x = "n" ]] || [[ $x = "N" ]] ; do
+read -p "Enter URL ${i}: " url
+echo $url >> dl
+i=`expr $i + 1`
+read -p "Done ? (y/n)" x
+done
+
+echo "Downloading..."
+
+for i in `cat dl` ; do
+youtube-dl -f 'bestaudio[ext=m4a]' $i
+done
+
+echo "Converting..."
+
+for f in *.m4a; do
+ffmpeg -i "$f" -codec:v copy -codec:a libmp3lame -q:a 2 "${f%.m4a}.mp3";
+done
+
+if [ $? -eq 0 ]; then
+rm -f *.m4a
+fi
+
+rm -f dl
+
+echo "Done :D"
